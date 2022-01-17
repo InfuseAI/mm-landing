10485a2f2ffb0b07633926d7950eb707f98a084c
catcatcatcat <catcat1027@gmail.com>
2022-01-17 19:22:02 +0800

    add fourth section, change logo, modify section titles and discriptions

--- components/coffee.js
+++ components/coffee.js
@@ -95,7 +95,47 @@ export default function Coffee () {
             </div>
           </div>
         </div>
-
+      </div>
+      <div className="relative">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <div>
+              <div>
+                <span className="h-12 w-12 rounded-md flex items-center justify-center"
+                  style={{backgroundColor: "#365abd"}}
+                >
+                  <InboxIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  {t('section-fourth-title')}
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  {t('section-fourth-content')}
+                </p>
+                <div className="mt-6">
+                  <a
+                    href={CFUrl}
+                    className="inline-flex bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"
+                    style={{backgroundColor: "#365abd"}}
+                  >
+                  {t('section-fourth-action')}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0">
+            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                src={t('section-fourth-img')}
+                alt={t('section-fourth-img-alt')}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
