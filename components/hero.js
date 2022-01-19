f21e37411fa45a8919950e6fb6ea3bfc31adaffa
hbshih <hbshih@gmail.com>
2022-01-19 13:20:31 +0800

    added hero and images

--- components/hero.js
+++ components/hero.js
@@ -14,15 +14,6 @@ export default function Hero () {
     <div className="relative overflow-hidden" style={{backgroundColor: '#f3f7fe'}}>
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32" style={{backgroundColor: '#f3f7fe'}}>
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="#f3f7fe"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
 
           <Popover>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
